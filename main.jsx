35249b71d423b0150567fed9018ec7285c203e7d
borelMomha657 <borelmomha@gmail.com>
2026-06-11 13:14:57 +0100

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SonghoGame from "./SonghoGame.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SonghoGame />
-  </React.StrictMode>
-);
